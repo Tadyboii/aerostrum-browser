373f43f02f497af11d61ce7f757af738928d2587
Thaddeus Rosales <thadyboygwapo82@gmail.com>
2025-11-04 17:28:04 +0800

    addimages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import React from "react";
 import Cam from "./components/Cam";
 
 export default function App() {
+  const chords = ["C", "Dm", "Em", "F", "G", "Am", "Bdim"];
+
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      {/* <h1>🎸 AeroStrum Virtual Guitar</h1> */}
-      <Cam />
+    <div className="main-container">
+      {/* Left: Distorted */}
+      <div className="side-panel left-panel">
+        {chords.map((chord) => (
+          <img
+            key={chord}
+            src={`/aerostrum-browser/handpositions/distorted/${chord}.png`}
+            alt={`${chord} distorted`}
+            className="chord-image"
+          />
+        ))}
+      </div>
+
+      {/* Center: Camera */}
+      <div className="camera-panel">
+        <Cam />
+      </div>
+
+      {/* Right: Acoustic */}
+      <div className="side-panel right-panel">
+        {chords.map((chord) => (
+          <img
+            key={chord}
+            src={`/aerostrum-browser/handpositions/acoustic/${chord}.png`}
+            alt={`${chord} acoustic`}
+            className="chord-image"
+          />
+        ))}
+      </div>
     </div>
   );
 }
